20f56a8aa0c549189924a039219f030aadeeece5
ShebinSp <shebinsp@gmail.com>
2024-03-05 18:43:35 +0530

    client side added

--- server.go
+++ server.go
@@ -3,8 +3,9 @@ package main
 import (
 	"log"
 	"net"
-	"google.golang.org/grpc"
+
 	"github.com/ShebinSp/grpc_server/chat"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -13,8 +14,11 @@ func main() {
 		log.Fatalf("Failed to listern on port 9000: %v", err)
 	}
 
+	s := chat.Server{}
 	grpcServer := grpc.NewServer()
+	chat.RegisterChatServiceServer(grpcServer, &s)
 
+	
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
 	}
